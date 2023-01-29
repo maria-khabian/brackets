d4d69b2f498bcdd341003a7684bba124a9bcd757
maria-khabian <mariakhabian@gmail.com>
2023-01-29 20:14:52 +0100

    feat: solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const bracketsConfigOBJ = {}
+  Object.values(bracketsConfig).forEach((item, idx, arr) => bracketsConfigOBJ[item[0]] = item[1])
+  const check = []
+  for ( let i = 0 ; i < str.length ; i++ ) {
+    if (Object.values(bracketsConfigOBJ).includes(str[i])) {
+      let idx = Object.values(bracketsConfigOBJ).findIndex(item => item == str[i]);
+      check.pop(Object.keys(bracketsConfigOBJ)[idx])
+    } else if(Object.keys(bracketsConfigOBJ).includes(str[i])) {
+      check.push(str[i])
+    } 
+}
+  return check.length === 0 ? true : false
 }
