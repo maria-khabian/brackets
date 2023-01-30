e6e30b32a4a816122007094f08e8869bbedf14d6
maria-khabian <mariakhabian@gmail.com>
2023-01-30 12:55:48 +0100

    feat: end solution brackets

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
   const bracketsConfigOBJ = {}
+  
   Object.values(bracketsConfig).forEach((item, idx, arr) => bracketsConfigOBJ[item[0]] = item[1])
+  const arrKeys = Object.keys(bracketsConfigOBJ)
+  const arrValues = Object.values(bracketsConfigOBJ)
   const check = []
+  
   for ( let i = 0 ; i < str.length ; i++ ) {
-    if (Object.values(bracketsConfigOBJ).includes(str[i])) {
-      let idx = Object.values(bracketsConfigOBJ).findIndex(item => item == str[i]);
-      check.pop(Object.keys(bracketsConfigOBJ)[idx])
-    } else if(Object.keys(bracketsConfigOBJ).includes(str[i])) {
-      check.push(str[i])
-    } 
+    
+    let currentValue = str[i]
+    
+    if (arrKeys.includes(currentValue) && !(check.includes(currentValue) && bracketsConfigOBJ[currentValue] === currentValue)) {
+      
+      check.push(currentValue) 
+      
+    } else {
+      if (arrValues.includes(currentValue)) {
+    
+        let idx = arrValues.findIndex(item => item === currentValue);
+        
+          if(check[check.length-1] === arrKeys[idx]) {
+            check.pop()
+          } else {
+            return false
+          }
+      }   
+    }
+     
 }
-  return check.length === 0 ? true : false
+  return check.length === 0 
 }
